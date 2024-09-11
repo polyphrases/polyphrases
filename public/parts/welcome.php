1a8f944697e0b2c669e4e9c66048b40ac49023ff
Alvaro Franz <mail@alvarofranz.com>
2024-09-11 21:30:50 +0200

    Change welcome wording

--- public/parts/welcome.php
+++ public/parts/welcome.php
@@ -1,7 +1,7 @@
 <div class="flex-me-down">
     <h1>Welcome to Poly Phrases!</h1>
-    <p>Poly Phrases is your daily language booster, designed to keep your skills sharp and your vocabulary growing. Each day, you’ll receive a fresh and imaginative phrase in multiple languages, delivered straight to your inbox.</p>
+    <p>A polyglots tool, designed to keep your skills sharp and your vocabulary growing. Each day, a fresh and imaginative phrase is generated in multiple languages.</p>
     <p>With Poly Phrases, you’ll encounter a variety of words and expressions in fun and surprising contexts, making it easier to remember and use new vocabulary in everyday conversation. It's an effortless and enjoyable way to stay connected with the languages you love.</p>
-    <p>Ready to add some excitement to your language learning? Click below to subscribe and start expanding your multilingual horizons today!</p>
+    <p>Ready to add some excitement to your language learning? Click below to subscribe to the free daily phrases service and start expanding your multilingual horizons today!</p>
     <p class="flex-justify-center"><a class="button" href="/subscribe">Subscribe for Free</a></p>
 </div>
