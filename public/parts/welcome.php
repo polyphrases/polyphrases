cf37537aa75244f582d42b54ec656bc5ebef03ba
Alvaro Franz <mail@alvarofranz.com>
2024-09-11 21:31:20 +0200

    Change welcome wording again

--- public/parts/welcome.php
+++ public/parts/welcome.php
@@ -3,5 +3,5 @@
     <p>A polyglots tool, designed to keep your skills sharp and your vocabulary growing. Each day, a fresh and imaginative phrase is generated in multiple languages.</p>
     <p>With Poly Phrases, you’ll encounter a variety of words and expressions in fun and surprising contexts, making it easier to remember and use new vocabulary in everyday conversation. It's an effortless and enjoyable way to stay connected with the languages you love.</p>
     <p>Ready to add some excitement to your language learning? Click below to subscribe to the free daily phrases service and start expanding your multilingual horizons today!</p>
-    <p class="flex-justify-center"><a class="button" href="/subscribe">Subscribe for Free</a></p>
+    <p class="flex-justify-center"><a class="button" href="/subscribe">I want some Phrases!</a></p>
 </div>
